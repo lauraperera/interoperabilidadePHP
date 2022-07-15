d6a56496587ec0ae1b7938f2624f9fbc612c6d23
lauraperera <lauraperera.pss@gmail.com>
2022-07-15 17:30:31 -0300

    funcionou aqui

--- farmaciaEspanha/venda_insert.php
+++ farmaciaEspanha/venda_insert.php
@@ -10,7 +10,7 @@
 	$anvisa = $resultado[0]['anvisa'];
 	$cpf = $resultado[0]['cpf'];
 
-	//error_log(var_export($resultado, true));
+	error_log(var_export($resultado, true));
 
 	$array = ['anvisa' => $anvisa, 'cpf' => $cpf];
 	$json = json_encode($array);
@@ -22,24 +22,26 @@
 	$json = curl_exec($client);
 	$array = json_decode($json, true);
 
-	$select ="select p.anvisa as anvisa, c.cpf as cpf from venda join produto p on p.id = produto join cliente c on c.id = cliente where cliente = '".$_REQUEST['cliente']."' ";
-	$resultado = $conexao->query($select)->fetchAll();
+	$select2 ="select p.anvisa as anvisa, c.cpf as cpf from venda join produto p on p.id = produto join cliente c on c.id = cliente where cliente = '".$_REQUEST['cliente']."' ";
+	$resultado2 = $conexao->query($select2)->fetchAll();
 
-	$anvisa = $resultado[0]['anvisa'];
-	$cpf = $resultado[0]['cpf'];
+	$anvisa2 = $resultado2[0]['anvisa'];
+	$cpf2 = $resultado2[0]['cpf'];
 
 	//error_log(var_export($resultado, true));
 
-	$array = ['anvisa' => $anvisa, 'cpf' => $cpf];
-	$json = json_encode($array);
+	$array2 = ['anvisa' => $anvisa2, 'cpf' => $cpf2];
+	$json2 = json_encode($array2);
 
 	$client2 = curl_init('http://localhost:8082/servico.php');
-	curl_setopt($client2, CURLOPT_POSTFIELDS, $json);
+	curl_setopt($client2, CURLOPT_POSTFIELDS, $json2);
 	curl_setopt($client2, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
 	curl_setopt($client2, CURLOPT_RETURNTRANSFER, true);
-	$json = curl_exec($client2);
-	$array2 = json_decode($json, true);
-	print $json;
+	$json2 = curl_exec($client2);
+	$array2 = json_decode($json2, true);
+	
+	print $json2;
+
 	if($array['status'] == 'sucesso' || $array2['status'] == 'sucesso'){
 		$sql = " insert into venda values (null, '" . $_REQUEST['produto'] . "', '" . $_REQUEST['cliente'] . "', datetime('now'), ( select (valor * 0.9) from produto where id = '" . $_REQUEST['produto'] . "') ); ";
 		$conexao->exec($sql);
